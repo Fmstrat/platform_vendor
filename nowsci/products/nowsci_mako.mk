5522d97ca6a9a31c0df1fbb2eadc4812ea7d42f3
Fmstrat <github@nosolutions.com>
2014-11-18 22:48:26 -0500

    Build.prop edits to fix location and other things.

--- nowsci/products/nowsci_mako.mk
+++ nowsci/products/nowsci_mako.mk
@@ -11,6 +11,24 @@ PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
+# Common build prop overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    keyguard.no_require_sim=true \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
+
 # For backups
 PRODUCT_COPY_FILES += \
     vendor/nowsci/prebuilts/common/system/addon.d/50-nowsci.sh:system/addon.d/50-nowsci.sh:lge \
